b74c19f3e37fab3849a0b50c5bf9a8a79f4a5fa5
Suyash Pandit <suyashpandit16@gmail.com>
2024-03-28 15:59:10 -0700

    More complex practice

--- Practice.java
+++ Practice.java
@@ -1,7 +1,15 @@
+import java.util.*;
+
 public class Practice {
     public static void main(String[] args) {
+        Set<Integer> nums = new TreeSet<>();
         for (int i=0; i<15; i++) {
             System.out.println(i + "  " + i*i + "  " + i*i*i);
+            nums.add(i);
+            nums.add(i*i);
+            nums.add(i*i*i);
         }
+
+        System.out.println(nums.size());
     }
 }
