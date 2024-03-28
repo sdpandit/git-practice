45e9d959f208e79b2cf70618b6cb733d26250637
Suyash Pandit <suyashpandit16@gmail.com>
2024-03-28 15:54:44 -0700

    First commit

---
+++ Practice.java
@@ -0,0 +1,7 @@
+public class Practice {
+    public static void main(String[] args) {
+        for (int i=0; i<15; i++) {
+            System.out.println(i + "  " + i*i + "  " + i*i*i);
+        }
+    }
+}
